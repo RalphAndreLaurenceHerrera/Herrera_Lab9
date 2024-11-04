841d659c7704958cee764c809b82cbbd4d3ee556
Ralph Andre Laurence Herrera <laurenceherrera90@gmail.com>
2024-11-05 00:48:06 +0800

    Update PatternsCode.py

--- PatternsCode.py
+++ PatternsCode.py
@@ -1,10 +1,12 @@
 # Asks the number of rows from the user.
-rows = int(input("Enter the number of rows: "))
+rowswant = int(input("Enter the number of rows you want to show: "))
 # Starting number
 num = 1
 # Pattern Code
-for i in range(1, rows + 1):
-    for x in range(i):
+# Is rowscount ≤ rowswanted?
+for rowscount in range(1, rowswant + 1):
+    # Is columncount < rowscount?
+    for columnscount in range(rowscount):
         print(num, end=" ")
         num += 1
     print()
